fdda987278c27dfabbca7881059f2871a3a4111b
nandishpatel1996 <1418beit30031@gmail.com>
2021-01-11 22:57:30 +1100

    Solutions added - Join and Filtering

--- Udacity_SQL/SQL_notes_UDACITY.sql
+++ Udacity_SQL/SQL_notes_UDACITY.sql
@@ -462,6 +462,24 @@ the account name. Sort the accounts alphabetically (A-Z) according to account
 name.
 */
 
+SELECT r.name region_name, a.name account_name, s.name sales_rep_name
+FROM region r 
+JOIN sales_reps s
+ON r.id = s.region_id  AND r.name = 'Midwest'
+JOIN accounts a
+ON s.id = a.sales_rep_id
+ORDER BY a.name; /*ORDER BY account_name*/
+
+SELECT r.name region_name, a.name account_name, s.name sales_rep_name
+FROM region r 
+JOIN sales_reps s
+ON r.id = s.region_id 
+JOIN accounts a
+ON s.id = a.sales_rep_id
+WHERE r.name = 'Midwest'
+ORDER BY a.name; /*ORDER BY account_name*/
+
+
 /*
 2. Provide a table that provides the region for each sales_rep along with 
 their associated accounts. This time only for accounts where the sales rep 
@@ -469,6 +487,25 @@ has a first name starting with S and in the Midwest region. Your final table
 should include three columns: the region name, the sales rep name, and the 
 account name. Sort the accounts alphabetically (A-Z) according to account name.
 */
+
+SELECT r.name region_name, a.name account_name, s.name sales_rep_name
+FROM region r 
+JOIN sales_reps s
+ON r.id = s.region_id  AND (s.name LIKE 'S%' AND r.name = 'Midwest')
+JOIN accounts a
+ON s.id = a.sales_rep_id
+ORDER BY a.name; /*ORDER BY account_name*/
+
+
+SELECT r.name region_name, a.name account_name, s.name sales_rep_name
+FROM region r 
+JOIN sales_reps s
+ON r.id = s.region_id 
+JOIN accounts a
+ON s.id = a.sales_rep_id
+WHERE r.name = 'Midwest' AND s.name LIKE 'S%'
+ORDER BY a.name; /*ORDER BY account_name*/
+
 
 /*
 3. Provide a table that provides the region for each sales_rep along with 
@@ -478,6 +515,25 @@ should include three columns: the region name, the sales rep name, and the
 account name. Sort the accounts alphabetically (A-Z) according to account name.
 */
 
+SELECT r.name region_name, a.name account_name, s.name sales_rep_name
+FROM region r 
+JOIN sales_reps s
+ON r.id = s.region_id  AND (s.name LIKE '% K%' AND r.name = 'Midwest')
+JOIN accounts a
+ON s.id = a.sales_rep_id
+ORDER BY a.name; /*ORDER BY account_name*/
+
+
+SELECT r.name region_name, a.name account_name, s.name sales_rep_name
+FROM region r 
+JOIN sales_reps s
+ON r.id = s.region_id 
+JOIN accounts a
+ON s.id = a.sales_rep_id
+WHERE r.name = 'Midwest' AND s.name LIKE '% K%'
+ORDER BY a.name; /*ORDER BY account_name*/
+
+
 /*
 4. Provide the name for each region for every order, as well as the account 
 name and the unit price they paid (total_amt_usd/total) for the order. However,
@@ -486,6 +542,30 @@ Your final table should have 3 columns: region name, account name, and unit
 price. In order to avoid a division by zero error, adding .01 to the 
 denominator here is helpful total_amt_usd/(total+0.01).
 */
+
+SELECT r.name region_name, a.name account_name, 
+o.total_amt_usd/(o.total + 0.001) unit_price
+FROM region r 
+JOIN sales_reps s
+ON r.id = s.region_id
+JOIN accounts a
+ON s.id = a.sales_rep_id
+JOIN orders o
+ON a.id = o.account_id AND o.standard_qty > 100
+ORDER BY unit_price;
+
+
+SELECT r.name region_name, a.name account_name, 
+o.total_amt_usd/(o.total + 0.001) unit_price
+FROM region r 
+JOIN sales_reps s
+ON r.id = s.region_id
+JOIN accounts a
+ON s.id = a.sales_rep_id
+JOIN orders o
+ON a.id = o.account_id
+WHERE o.standard_qty > 100
+ORDER BY unit_price;
 
 /*
 5. Provide the name for each region for every order, as well as the account 
@@ -497,6 +577,32 @@ unit price first. In order to avoid a division by zero error, adding .01 to
 the denominator here is helpful (total_amt_usd/(total+0.01).
 */
 
+SELECT r.name region_name, a.name account_name, 
+o.total_amt_usd/(o.total + 0.001) unit_price
+FROM region r 
+JOIN sales_reps s
+ON r.id = s.region_id
+JOIN accounts a
+ON s.id = a.sales_rep_id
+JOIN orders o
+ON a.id = o.account_id AND o.standard_qty > 100 AND o.poster_qty > 50
+ORDER BY unit_price;
+
+
+SELECT r.name region_name, a.name account_name, 
+o.total_amt_usd/(o.total + 0.001) unit_price
+FROM region r 
+JOIN sales_reps s
+ON r.id = s.region_id
+JOIN accounts a
+ON s.id = a.sales_rep_id
+JOIN orders o
+ON a.id = o.account_id
+WHERE o.standard_qty > 100 AND o.poster_qty > 50
+ORDER BY unit_price;
+
+
+
 /*
 6. Provide the name for each region for every order, as well as the account 
 name and the unit price they paid (total_amt_usd/total) for the order. However,
@@ -507,13 +613,71 @@ price first. In order to avoid a division by zero error, adding .01 to the
 denominator here is helpful (total_amt_usd/(total+0.01).
 */
 
+SELECT r.name region_name, a.name account_name, 
+o.total_amt_usd/(o.total + 0.001) unit_price
+FROM region r 
+JOIN sales_reps s
+ON r.id = s.region_id
+JOIN accounts a
+ON s.id = a.sales_rep_id
+JOIN orders o
+ON a.id = o.account_id AND o.standard_qty > 100 AND o.poster_qty > 50
+ORDER BY unit_price DESC;
+
+
+SELECT r.name region_name, a.name account_name, 
+o.total_amt_usd/(o.total + 0.001) unit_price
+FROM region r 
+JOIN sales_reps s
+ON r.id = s.region_id
+JOIN accounts a
+ON s.id = a.sales_rep_id
+JOIN orders o
+ON a.id = o.account_id
+WHERE o.standard_qty > 100 AND o.poster_qty > 50
+ORDER BY unit_price DESC;
+
+
 /*
 7. What are the different channels used by account id 1001? Your final table 
 should have only 2 columns: account name and the different channels. You can 
 try SELECT DISTINCT to narrow down the results to only the unique values.
 */
 
+SELECT w.channel, a.name
+FROM web_events w
+JOIN accounts a
+ON w.account_id = a.id AND a.id = 1001;
+
+/* BETTER */
+
+SELECT DISTINCT w.channel, a.name
+FROM web_events w
+JOIN accounts a
+ON w.account_id = a.id AND a.id = 1001;
+
+
+SELECT DISTINCT w.channel, a.name
+FROM web_events w
+JOIN accounts a
+ON w.account_id = a.id
+WHERE a.id = 1001;
+
+
 /*
 8. Find all the orders that occurred in 2015. Your final table should have 4 
 columns: occurred_at, account name, order total, and order total_amt_usd.
 */
+
+
+SELECT o.occurred_at, a.name, o.total, o.total_amt_usd
+FROM orders o
+JOIN accounts a
+ON o.account_id = a.id AND o.occurred_at BETWEEN '01-01-2015' AND '01-01-2016';
+
+SELECT o.occurred_at, a.name, o.total, o.total_amt_usd
+FROM orders o
+JOIN accounts a
+ON o.account_id = a.id 
+WHERE o.occurred_at BETWEEN '01-01-2015' AND '01-01-2016';
+
