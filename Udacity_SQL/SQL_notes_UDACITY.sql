873dcd177b36fe1419198be9e024bbf4f2bd3671
nandishpatel1996 <1418beit30031@gmail.com>
2021-01-12 16:48:22 +1100

    Solution added - Min, Max, Avg

--- Udacity_SQL/SQL_notes_UDACITY.sql
+++ Udacity_SQL/SQL_notes_UDACITY.sql
@@ -740,19 +740,35 @@ FROM orders;
 need to return the date.
 */
 
+SELECT MIN(occurred_at) 
+FROM orders;
+
 /*
 2. Try performing the same query as in question 1 
 without using an aggregation function.
 */
 
+SELECT occurred_at
+FROM orders
+ORDER BY occurred_at
+LIMIT 1;
+
 /*
 3. When did the most recent (latest) web_event occur?
 */
+
+SELECT MAX(occurred_at)
+FROM web_events;
 
 /*
 4. Try to perform the result of the previous query 
 without using an aggregation function.
 */
+
+SELECT occurred_at
+FROM web_events
+ORDER BY occurred_at DESC
+LIMIT 1;
 
 /*
 5. Find the mean (AVERAGE) amount spent per order on 
@@ -762,9 +778,25 @@ should have 6 values - one for each paper type for the
 average number of sales, as well as the average amount.
 */
 
+SELECT AVG(standard_qty) mean_standard, 
+AVG(gloss_qty) mean_gloss, 
+AVG(poster_qty) mean_poster, 
+AVG(standard_amt_usd) mean_standard_usd, 
+AVG(gloss_amt_usd) mean_gloss_usd, 
+AVG(poster_amt_usd) mean_poster_usd
+FROM orders;
+
 /*
 6. Via the video, you might be interested in how to 
 calculate the MEDIAN. Though this is more advanced than 
 what we have covered so far try finding - what is the 
 MEDIAN total_usd spent on all orders?
 */
+
+SELECT *
+FROM (SELECT total_amt_usd
+      FROM orders
+      ORDER BY total_amt_usd
+      LIMIT 3457) AS Table1
+ORDER BY total_amt_usd DESC
+LIMIT 2;
