6f758fbdd2b8b7cb90ab1602ce8f25ec76de1997
nandishpatel1996 <1418beit30031@gmail.com>
2021-01-12 16:12:26 +1100

    Solutions added - Aggregation

--- Udacity_SQL/SQL_notes_UDACITY.sql
+++ Udacity_SQL/SQL_notes_UDACITY.sql
@@ -686,20 +686,30 @@ WHERE o.occurred_at BETWEEN '01-01-2015' AND '01-01-2016';
 
 ### Aggregation Questions
 
+
 /*
 1. Find the total amount of poster_qty paper ordered in 
 the orders table.
 */
+
+SELECT SUM(poster_qty) total_poster_qty
+FROM orders;
 
 /*
 2. Find the total amount of standard_qty paper ordered 
 in the orders table.
 */
 
+SELECT SUM(standard_qty) total_standard_qty
+FROM orders;
+
 /*
 3. Find the total dollar amount of sales using the 
 total_amt_usd in the orders table.
 */
+
+SELECT SUM(total_amt_usd) total_amount_usd
+FROM orders;
 
 /*
 4. Find the total amount spent on standard_amt_usd and 
@@ -708,8 +718,16 @@ This should give a dollar amount for each order in the
 table.
 */
 
+SELECT SUM(standard_amt_usd) std_amount_usd, 
+SUM(gloss_amt_usd) gloss_amount_usd
+FROM orders;
+
 /*
 5. Find the standard_amt_usd per unit of standard_qty 
 paper. Your solution should use both an aggregation and 
 a mathematical operator.
 */
+
+SELECT SUM(standard_amt_usd)/SUM(standard_qty) 
+AS unit_price_standard
+FROM orders;
