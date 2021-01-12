e3df8ce254dfb582b213c9f29b40ef0bf91d0577
nandishpatel1996 <1418beit30031@gmail.com>
2021-01-12 19:58:31 +1100

    Question added - Group By

--- Udacity_SQL/SQL_notes_UDACITY.sql
+++ Udacity_SQL/SQL_notes_UDACITY.sql
@@ -800,3 +800,59 @@ FROM (SELECT total_amt_usd
       LIMIT 3457) AS Table1
 ORDER BY total_amt_usd DESC
 LIMIT 2;
+
+
+### Questions: GROUP BY
+
+
+
+/*
+1. Which account (by name) placed the earliest order? 
+Your solution should have the account name and the date 
+of the order.
+*/
+
+
+/*
+2. Find the total sales in usd for each account. You 
+should include two columns - the total sales for each 
+company's orders in usd and the company name.
+*/
+
+
+/*
+3. Via what channel did the most recent (latest) 
+web_event occur, which account was associated with this 
+web_event? Your query should return only three values - 
+the date, channel, and account name.
+*/
+
+
+/*
+4. Find the total number of times each type of channel 
+from the web_events was used. Your final table should 
+have two columns - the channel and the number of times 
+the channel was used.
+*/
+
+
+/*
+5. Who was the primary contact associated with the 
+earliest web_event?
+*/
+
+
+/*
+6. What was the smallest order placed by each account 
+in terms of total usd. Provide only two columns - the 
+account name and the total usd. Order from smallest 
+dollar amounts to largest.
+*/
+
+
+/*
+7. Find the number of sales reps in each region. Your 
+final table should have two columns - the region and 
+the number of sales_reps. Order from fewest reps to 
+most reps.
+*/
