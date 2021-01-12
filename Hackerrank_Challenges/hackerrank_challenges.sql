93c047b6fa8c1e7c7cb996dfc749755c09d24555
nandishpatel1996 <1418beit30031@gmail.com>
2021-01-12 22:24:11 +1100

    Challenge 3 Easy - Average Population of Each Continent

--- Hackerrank_Challenges/hackerrank_challenges.sql
+++ Hackerrank_Challenges/hackerrank_challenges.sql
@@ -1,10 +1,11 @@
 ## 1. Asian Population
 
 /*
-Given the CITY and COUNTRY tables, query the sum of the populations of all 
-cities where the CONTINENT is 'Asia'.
+Given the CITY and COUNTRY tables, query the sum of the 
+populations of all cities where the CONTINENT is 'Asia'.
 
-Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+Note: CITY.CountryCode and COUNTRY.Code are matching key 
+columns.
 */
 
 
@@ -18,10 +19,11 @@ WHERE co.CONTINENT = 'Asia';
 ## 2. African Cities
 
 /*
-Given the CITY and COUNTRY tables, query the names of all cities where the 
-CONTINENT is 'Africa'.
+Given the CITY and COUNTRY tables, query the names of all 
+cities where the CONTINENT is 'Africa'.
 
-Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+Note: CITY.CountryCode and COUNTRY.Code are matching key 
+columns.
 */
 
 
@@ -30,3 +32,25 @@ FROM COUNTRY co
 JOIN CITY ci
 ON ci.COUNTRYCODE = co.CODE
 WHERE co.CONTINENT = 'Africa';
+
+
+## 3. Average Population of Each Continent
+
+/*
+Given the CITY and COUNTRY tables, query the names of all
+the continents (COUNTRY.Continent) and their respective 
+average city populations (CITY.Population) rounded down 
+to the nearest integer.
+
+Note: CITY.CountryCode and COUNTRY.Code are matching key 
+columns.
+*/
+
+
+SELECT co.CONTINENT, floor(AVG(ci.POPULATION))
+FROM COUNTRY co
+JOIN CITY ci
+ON ci.COUNTRYCODE = co.CODE
+GROUP BY co.CONTINENT;
+
+
