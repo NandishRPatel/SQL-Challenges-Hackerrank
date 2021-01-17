78eecf826e633db15bea80b0006e1f12cd449f17
nandishpatel1996 <1418beit30031@gmail.com>
2021-01-17 22:27:04 +1100

    Minor formatting changes

--- Udacity_SQL/SQL_notes_UDACITY.sql
+++ Udacity_SQL/SQL_notes_UDACITY.sql
@@ -1,198 +1,193 @@
 ### LIMIT
 
 /*
-1. Try using LIMIT yourself below by writing a query that displays all the data 
-in the occurred_at, account_id, and channel columns of the web_events 
-table, and limits the output to only the first 15 rows.
+1. Try using LIMIT yourself below by writing a query 
+that displays all the data in the occurred_at, 
+account_id, and channel columns of the web_events table, 
+and limits the output to only the first 15 rows.
 */
 
 SELECT occurred_at, account_id, channel 
 FROM web_events
-LIMIT(15);
+LIMIT(15); # LIMIT 15
+
+
 
 
 ### ORDER BY
 
 /*
-1. Write a query to return the 10 earliest orders in the orders table. Include 
-the id, occurred_at, and total_amt_usd.
+1. Write a query to return the 10 earliest orders in 
+the orders table. Include the id, occurred_at, and 
+total_amt_usd.
 */
 
 SELECT id, occurred_at, total_amt_usd 
 FROM orders
-ORDER BY occurred_at
-LIMIT(10);
-
-
-SELECT id, occurred_at, total_amt_usd 
-FROM orders
-ORDER BY 2
-LIMIT(10);
+ORDER BY occurred_at # ORDER BY 2
+LIMIT(10); # LIMIT 10
 
 
 /*
-2. Write a query to return the top 5 orders in terms of largest total_amt_usd. 
-Include the id, account_id, and total_amt_usd.
-*/
-
-
-SELECT id, account_id, total_amt_usd
-FROM orders
-ORDER BY total_amt_usd DESC
-LIMIT(5);
-
-
-SELECT id, account_id, total_amt_usd
-FROM orders
-ORDER BY 3 DESC
-LIMIT(5);
-
-
-/*
-3. Write a query to return the lowest 20 orders in terms of smallest 
-total_amt_usd. Include the id, account_id, and total_amt_usd.
+2. Write a query to return the top 5 orders in terms of 
+largest total_amt_usd. Include the id, account_id, and 
+total_amt_usd.
 */
 
 SELECT id, account_id, total_amt_usd
 FROM orders
-ORDER BY total_amt_usd
-LIMIT(20);
+ORDER BY total_amt_usd DESC # ORDER BY 3 DESC
+LIMIT(5); # LIMIT 5
 
+
+/*
+3. Write a query to return the lowest 20 orders in 
+terms of smallest total_amt_usd. Include the id, 
+account_id, and total_amt_usd.
+*/
 
 SELECT id, account_id, total_amt_usd
 FROM orders
-ORDER BY 3
-LIMIT(20);
+ORDER BY total_amt_usd # ORDER BY 3
+LIMIT(20); # LIMIT 20
+
+
 
 
 ### ORDER BY RETURNS
 
 /*
-1. Write a query that displays the order ID, account ID, and total dollar amount 
-for all the orders, sorted first by the account ID (in ascending order), and 
-then by the total dollar amount (in descending order).
+1. Write a query that displays the order ID, account 
+ID, and total dollar amount for all the orders, sorted 
+first by the account ID (in ascending order), and then 
+by the total dollar amount (in descending order).
 */
 
 SELECT id, account_id, total_amt_usd
 FROM orders
-ORDER BY account_id, total_amt_usd DESC;
-
-
-SELECT id, account_id, total_amt_usd
-FROM orders
-ORDER BY 2, 3 DESC;
+ORDER BY account_id, 
+total_amt_usd DESC; # ORDER BY 2, 3 DESC
 
 
 /*
-2. Now write a query that again displays order ID, account ID, and total dollar 
-amount for each order, but this time sorted first by total dollar amount (in 
-descending order), and then by account ID (in ascending order).
+2. Now write a query that again displays order ID, 
+account ID, and total dollar amount for each order, but 
+this time sorted first by total dollar amount (in 
+descending order), and then by account ID (in 
+ascending order).
 */
 
 SELECT id, account_id, total_amt_usd
 FROM orders
-ORDER BY total_amt_usd DESC, account_id;
-
-
-SELECT id, account_id, total_amt_usd
-FROM orders
-ORDER BY 3 DESC, 2;
+ORDER BY total_amt_usd DESC, 
+account_id; # ORDER BY 3 DESC, 2
 
 
 /*
-3. Compare the results of these two queries above. How are the results different 
-when you switch the column you sort on first?
+3. Compare the results of these two queries above. How 
+are the results different when you switch the column 
+you sort on first?
 */
 
 /*
-In query #1, all of the orders for each account ID are grouped together, and 
-then within each of those groupings, the orders appear from the greatest order 
-mount to the least. In query #2, since you sorted by the total dollar amount 
-first, the orders appear from greatest to least regardless of which account ID 
-hey were from. Then they are sorted by account ID next. (The secondary sorting
-by account ID is difficult to see here, since only if there were two orders 
-with equal total dollar amounts would there need to be any sorting by account 
-ID.)
+In query #1, all of the orders for each account ID are 
+grouped together, and then within each of those 
+groupings, the orders appear from the greatest order 
+mount to the least. In query #2, since you sorted by 
+the total dollar amount first, the orders appear from 
+greatest to least regardless of which account ID hey 
+were from. Then they are sorted by account ID next. 
+(The secondary sorting by account ID is difficult to 
+see here, since only if there were two orders with 
+equal total dollar amounts would there need to be any 
+sorting by account ID.)
 */
+
+
 
 
 ### WHERE
 
 
 /*
-1. Pulls the first 5 rows and all columns from the orders table that have a 
-dollar amount of gloss_amt_usd greater than or equal to 1000.
+1. Pulls the first 5 rows and all columns from the 
+orders table that have a dollar amount of gloss_amt_usd 
+greater than or equal to 1000.
 */
 
 SELECT * 
 FROM orders
 WHERE gloss_amt_usd >= 1000
-LIMIT(5);
-
+LIMIT(5); # LIMIT 5
 
 
 /*
-2. Pulls the first 10 rows and all columns from the orders table that have a 
-total_amt_usd less than 500.
+2. Pulls the first 10 rows and all columns from the 
+orders table that have a total_amt_usd less than 500.
 */
-
 
 SELECT *
 FROM orders
 WHERE total_amt_usd < 500
-LIMIT(10);
+LIMIT(10); # LIMIT 10
+
+
 
 
 ### Practice Question Using WHERE with Non-Numeric Data
 
 
 /*
-1. Filter the accounts table to include the company name, website, and the 
-primary point of contact (primary_poc) just for the Exxon Mobil company in 
-the accounts table.
+1. Filter the accounts table to include the company 
+name, website, and the primary point of contact 
+(primary_poc) just for the Exxon Mobil company in the 
+accounts table.
 */
 
 SELECT name, website, primary_poc
 FROM accounts
-WHERE name = 'Exxon Mobil';
+WHERE name = 'Exxon Mobil'; 
+# WHERE name IN('Exxon Mobil')
 
-
-SELECT name, website, primary_poc
-FROM accounts
-WHERE name IN('Exxon Mobil');
 
 
 
 ### Questions using Arithmetic Operations
 
+
 /*
-1. Create a column that divides the standard_amt_usd by the standard_qty to 
-find the unit price for standard paper for each order. Limit the results to 
-the first 10 orders, and include the id and account_id fields.
+1. Create a column that divides the standard_amt_usd by 
+the standard_qty to find the unit price for standard 
+paper for each order. Limit the results to the first 10 
+orders, and include the id and account_id fields.
 */
 
 SELECT id, account_id, standard_amt_usd/standard_qty 
 AS unit_price
 FROM orders
-LIMIT(10);
+LIMIT(10); # LIMIT 10
 
 
 SELECT id, account_id, 
 round(standard_amt_usd/standard_qty, 2) AS unit_price
 FROM orders
-LIMIT(10);
+LIMIT(10); # LIMIT 10
 
 
 /*
-2. Write a query that finds the percentage of revenue that comes from poster 
-paper for each order. You will need to use only the columns that end with 
-_usd. (Try to do this without using the total column.) Display the id and 
-account_id fields also. NOTE - you will receive an error with the correct 
-solution to this question. This occurs because at least one of the values in 
-the data creates a division by zero in your formula. You will learn later in 
-the course how to fully handle this issue. For now, you can just limit your 
-calculations to the first 10 orders, as we did in question #1, and you'll avoid
- that set of data that causes the problem.
+2. Write a query that finds the percentage of revenue 
+that comes from poster paper for each order. You will 
+need to use only the columns that end with _usd. (Try 
+to do this without using the total column.) Display 
+the id and account_id fields also. NOTE - you will 
+receive an error with the correct solution to this 
+question. This occurs because at least one of the 
+values in the data creates a division by zero in your 
+formula. You will learn later in the course how to 
+fully handle this issue. For now, you can just limit 
+your calculations to the first 10 orders, as we did 
+in question #1, and you'll avoid that set of data that
+causes the problem.
 */
 
 SELECT id, account_id, (poster_amt_usd * 100)/(poster_amt_usd + standard_amt_usd + 
@@ -205,7 +200,10 @@ SELECT id, account_id, round((poster_amt_usd * 100)/(poster_amt_usd + standard_a
 FROM orders;
 
 
+
+
 ### Questions using the LIKE operator
+
 
 /*
 1. All the companies whose names start with 'C'.
@@ -215,6 +213,7 @@ SELECT *
 FROM accounts
 WHERE name LIKE 'C%';
 
+
 /*
 2. All companies whose names contain the string 'one' somewhere in the name.
 */
@@ -222,6 +221,7 @@ WHERE name LIKE 'C%';
 SELECT *
 FROM accounts
 WHERE name LIKE '%one%';
+
 
 /*
 3. All companies whose names end with 's'.
@@ -232,7 +232,10 @@ FROM accounts
 WHERE name LIKE '%s';
 
 
+
+
 ### Questions using IN operator
+
 
 /*
 1. Use the accounts table to find the account name, primary_poc, and 
@@ -242,6 +245,7 @@ sales_rep_id for Walmart, Target, and Nordstrom.
 SELECT name, primary_poc, sales_rep_id
 FROM accounts
 WHERE name IN ('Walmart', 'Target', 'Nordstrom');
+
 
 /*
 2. Use the web_events table to find all information regarding individuals who 
@@ -253,7 +257,10 @@ FROM web_events
 WHERE channel IN ('organic', 'adwords');
 
 
+
+
 ### Questions using the NOT operator
+
 
 /*
 1. Use the accounts table to find the account name, primary poc, and sales 
@@ -274,6 +281,7 @@ SELECT *
 FROM web_events
 WHERE channel NOT IN ('organic', 'adwords');
 
+
 /*
 3. All the companies whose names do not start with 'C'.
 */
@@ -281,6 +289,7 @@ WHERE channel NOT IN ('organic', 'adwords');
 SELECT *
 FROM accounts
 WHERE name NOT LIKE 'C%';
+
 
 /*
 4. All companies whose names do not contain the string 'one' somewhere in the 
@@ -291,6 +300,7 @@ SELECT *
 FROM accounts
 WHERE name NOT LIKE '%one%';
 
+
 /*
 5. All companies whose names do not end with 's'.
 */
@@ -300,7 +310,10 @@ FROM accounts
 WHERE name NOT LIKE '%s';
 
 
+
+
 ### Questions using AND and BETWEEN operators
+
 
 /*
 1. Write a query that returns all the orders where the standard_qty is over 
@@ -311,6 +324,7 @@ SELECT *
 FROM orders
 WHERE standard_qty > 1000 AND poster_qty = 0 AND gloss_qty = 0;
 
+
 /*
 2. Using the accounts table, find all the companies whose names do not start 
 with 'C' and end with 's'.
@@ -320,6 +334,7 @@ SELECT *
 FROM accounts
 WHERE (name NOT LIKE 'C%') AND (name NOT LIKE '%s');
 
+
 /*
 3. When you use the BETWEEN operator in SQL, do the results include the values 
 of your endpoints, or not? Figure out the answer to this important question by 
@@ -328,17 +343,10 @@ where gloss_qty is between 24 and 29. Then look at your output to see if the
 BETWEEN operator included the begin and end values or not.
 */
 
-
 SELECT occurred_at, gloss_qty
 FROM orders
 WHERE gloss_qty BETWEEN 24 AND 29
-ORDER BY gloss_qty DESC;
-
-
-SELECT occurred_at, gloss_qty
-FROM orders
-WHERE gloss_qty BETWEEN 24 AND 29
-ORDER BY 2 DESC;
+ORDER BY gloss_qty DESC; # ORDER BY 2 DESC
 
 
 /*
@@ -347,14 +355,16 @@ were contacted via the organic or adwords channels, and started their account
 at any point in 2016, sorted from newest to oldest.
 */
 
-
 SELECT *
 FROM web_events
 WHERE channel IN ('organic', 'adwords') AND 
 occurred_at BETWEEN '01-01-2016' AND '01-01-2017';
 
 
+
+
 ### Questions using the OR operator
+
 
 /*
 1. Find list of orders ids where either gloss_qty or poster_qty is greater than
@@ -364,6 +374,7 @@ occurred_at BETWEEN '01-01-2016' AND '01-01-2017';
 SELECT id
 FROM orders
 WHERE gloss_qty > 4000 OR poster_qty > 4000;
+
 
 /*
 2. Write a query that returns a list of orders where the standard_qty is zero
@@ -389,7 +400,9 @@ WHERE (name LIKE 'C%' OR name LIKE 'W%') AND
 
 
 
+
 ### JOIN
+
 
 /*
 1. Try pulling all the data from the accounts table, and all the data from 
@@ -412,6 +425,7 @@ accounts.website, accounts.primary_poc
 FROM orders
 JOIN accounts
 ON orders.account_id = accounts.id;
+
 
 
 
@@ -454,7 +468,6 @@ ON r.id = s.region_id
 JOIN accounts a 
 ON s.id = a.sales_rep_id
 ORDER BY account_name;
-
 
 
 SELECT r.name AS region_name, s.name AS sales_rep_name, 
