e43fa560c2f37330f515ef46ef7758b1988bbce6
nandishpatel1996 <1418beit30031@gmail.com>
2021-01-17 22:55:53 +1100

    Questions added - DATE Functions

--- Udacity_SQL/SQL_notes_UDACITY.sql
+++ Udacity_SQL/SQL_notes_UDACITY.sql
@@ -1436,3 +1436,89 @@ ON a.id = w.account_id
 GROUP BY 1, 2, 3 # GROUP BY a.id, a.name, w.channel
 ORDER BY 4 DESC # ORDER BY times_used
 LIMIT 20;
+
+
+
+
+### DATE FUNCTIONS
+
+
+/*
+GROUPing BY a date column is not usually very useful in 
+SQL, as these columns tend to have transaction data 
+down to a second. Keeping date information at such a 
+granular data is both a blessing and a curse, as it 
+gives really precise information (a blessing), but it 
+makes grouping information together directly difficult 
+(a curse).
+
+Lucky for us, there are a number of built in SQL 
+functions that are aimed at helping us improve our 
+experience in working with dates.
+
+The first function you are introduced to in working 
+with dates is DATE_TRUNC.
+
+DATE_TRUNC allows you to truncate your date to a 
+particular part of your date-time column. Common 
+trunctions are day, month, and year. Here is a great 
+blog post by Mode Analytics on the power of this 
+function.
+
+DATE_PART can be useful for pulling a specific portion 
+of a date, but notice pulling month or day of the week 
+(dow) means that you are no longer keeping the years 
+in order. Rather you are grouping for certain 
+components regardless of which year they belonged in.
+
+For additional functions you can use with dates, check 
+out the documentation here, but the DATE_TRUNC and 
+DATE_PART functions definitely give you a great start!
+*/
+
+
+/*
+1. Find the sales in terms of total dollars for all 
+orders in each year, ordered from greatest to least. 
+Do you notice any trends in the yearly sales totals?
+*/
+
+
+/* 
+When we look at the yearly totals, you might notice 
+that 2013 and 2017 have much smaller totals than all 
+other years. If we look further at the monthly data, 
+we see that for 2013 and 2017 there is only one month 
+of sales for each of these years (12 for 2013 and 1 
+for 2017). Therefore, neither of these are evenly 
+represented. Sales have been increasing year over year, 
+with 2016 being the largest sales to date. At this rate, 
+we might expect 2017 to have the largest sales.
+*/
+
+
+/*
+2. Which month did Parch & Posey have the greatest 
+sales in terms of total dollars? Are all months evenly 
+represented by the dataset?
+*/
+
+
+/*
+3. Which year did Parch & Posey have the greatest sales 
+in terms of total number of orders? Are all years 
+evenly represented by the dataset?
+*/
+
+
+/*
+4. Which month did Parch & Posey have the greatest 
+sales in terms of total number of orders? Are all 
+months evenly represented by the dataset?
+*/
+
+
+/*
+5. In which month of which year did Walmart spend the 
+most on gloss paper in terms of dollars?
+*/
