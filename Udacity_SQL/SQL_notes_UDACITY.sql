f8d4c69a083161b0b1c7b4178ec0fdc6ae5b2db0
NandishRPatel <1418beit30031@gmail.com>
2021-06-07 17:16:41 +1000

    Subquery Mania #6

--- Udacity_SQL/SQL_notes_UDACITY.sql
+++ Udacity_SQL/SQL_notes_UDACITY.sql
@@ -2034,10 +2034,17 @@ ORDER BY 2 DESC
 LIMIT 10) t1
 
 
-
 /*
 6. What is the lifetime average amount spent in terms 
 of total_amt_usd, including only the companies that 
 spent more per order, on average, than the average of 
 all orders.
 */
+
+SELECT AVG(avg_amt)
+FROM (SELECT o.account_id, AVG(o.total_amt_usd) avg_amt
+    FROM orders o
+    GROUP BY 1
+    HAVING AVG(o.total_amt_usd) > 
+    (SELECT AVG(o.total_amt_usd) avg_all
+           FROM orders o)) temp_table;
