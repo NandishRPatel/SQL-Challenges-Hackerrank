98af34a943398d9ae8765cf8be22e23427cb7062
NandishRPatel <1418beit30031@gmail.com>
2021-06-11 13:57:02 +1000

    SQL Clearning #1 - LEFT and RIGHT Questions

--- Udacity_SQL/SQL_notes_UDACITY.sql
+++ Udacity_SQL/SQL_notes_UDACITY.sql
@@ -2048,3 +2048,47 @@ FROM (SELECT o.account_id, AVG(o.total_amt_usd) avg_amt
     HAVING AVG(o.total_amt_usd) > 
     (SELECT AVG(o.total_amt_usd) avg_all
            FROM orders o)) temp_table;
+
+
+
+
+## SQL Data Cleaning
+
+
+
+### LEFT and RIGHT
+
+
+/*
+1. In the accounts table, there is a column holding the
+website for each company. The last three digits specify
+what type of web address they are using. A list of 
+extensions (and pricing) is provided here. Pull these 
+extensions and provide how many of each website type 
+exist in the accounts table.
+*/
+
+
+/*
+2. There is much debate about how much the name (or 
+even the first letter of a company name) matters. Use 
+the accounts table to pull the first letter of each 
+company name to see the distribution of company names 
+that begin with each letter (or number).
+*/
+
+
+/*
+3. Use the accounts table and a CASE statement to 
+create two groups: one group of company names that 
+start with a number and a second group of those company
+names that start with a letter. What proportion of 
+company names start with a letter? 
+*/
+
+
+/*
+4. Consider vowels as a, e, i, o, and u. What 
+proportion of company names start with a vowel, and 
+what percent start with anything else?
+*/
